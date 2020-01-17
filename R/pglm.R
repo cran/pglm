9f1855d53ed10eae900bb07f7bc7bf99165a5ff1
Yves Croissant <yves.croissant@univ-reunion.fr>
2020-01-17 16:00:06 +0000

    version 0.2-2

--- R/pglm.R
+++ R/pglm.R
@@ -16,14 +16,14 @@ pglm <-  function(formula, data, subset, na.action,
     dots <- list(...)
     args <- list(model = model, effect = effect)
     
-  if (is.character(family)){
-      if (family %in% c("ordprobit", "ordlogit", "tobit")){
-          if (family == "ordprobit") family <- list(family = "ordinal", link = "probit")
-          if (family == "ordlogit") family <- list(family = "ordinal", link = "logit")
-          if (family == "tobit") family <- list(family = "tobit", link = NULL)
-      }
-      else  family <- get(family, mode = "function")
-  }
+    if (is.character(family)){
+        if (family %in% c("ordprobit", "ordlogit", "tobit")){
+            if (family == "ordprobit") family <- list(family = "ordinal", link = "probit")
+            if (family == "ordlogit") family <- list(family = "ordinal", link = "logit")
+            if (family == "tobit") family <- list(family = "tobit", link = NULL)
+        }
+        else  family <- get(family, mode = "function")
+    }
     if (is.function(family)) family <- family()
     
     link <- thelink <- family$link
@@ -34,17 +34,12 @@ pglm <-  function(formula, data, subset, na.action,
     effect <- match.arg(effect)
     if (!any(is.na(model))) model <- match.arg(model)
 
-    # Check whether data is a pdata.frame and if not create it ; ignore
-    # this step if model = pooling and if index = NULL so that ordinary
-    # glm models can be fitted
-    if (model != "pooling" | ! is.null(index)){
-        if (inherits(data, "pdata.frame") && !is.null(index))
-            warning("the index argument is ignored because data is a pdata.frame")
-        if (!inherits(data, "pdata.frame")) data <- pdata.frame(data, index)
-        # Create a Formula object if necessary
-        if (! inherits(formula, "pFormula")) formula <- pFormula(formula)
-    }
-  
+    if (inherits(data, "pdata.frame") && !is.null(index))
+        warning("the index argument is ignored because data is a pdata.frame")
+    if (!inherits(data, "pdata.frame")) data <- pdata.frame(data, index)
+    if (! inherits(formula, "Formula")) formula <- as.Formula(formula)
+
+    
     # eval the model.frame
     cl <- match.call()
     mf <- match.call(expand.dots=FALSE)
@@ -52,8 +47,8 @@ pglm <-  function(formula, data, subset, na.action,
     mf <- mf[c(1,m)]
     mf$drop.unused.levels <- TRUE
     mf[[1]] <- as.name("model.frame")
-    mf$formula <- formula
-    mf$data <- data
+    mf$formula <- data
+    mf$data <- formula
     data <- eval(mf, parent.frame())
     # return the model.frame or estimate the model
     if (is.na(model)){
@@ -62,9 +57,9 @@ pglm <-  function(formula, data, subset, na.action,
     }
     Kw <- NULL
     if (model != "pooling"){
-        X <- model.matrix(formula, data, rhs = 1, model = "pooling", effect = effect)
+        X <- model.matrix(data, rhs = 1, model = "pooling", effect = effect)
         if (model == "within" && family == "poisson"){
-            Xw <- model.matrix(formula, data, rhs = 1, model = "within", effect = effect)
+            Xw <- model.matrix(data, rhs = 1, model = "within", effect = effect)
             Kw <- colnames(Xw)
             X <- X[, Kw, drop = FALSE]
         }
@@ -74,12 +69,11 @@ pglm <-  function(formula, data, subset, na.action,
         id <- attr(data, "index")[[1]]
     }
     else{
-        X <- model.matrix(formula, data)
+        X <- model.matrix(data)
         y <- model.response(data)
         if (inherits(data, "pdata.frame")) id <- attr(data, "index")[[1]]
         else id <- NULL
     }
-
     # compute the nodes and the weights for the gaussian quadrature
     if (model == "random" && (! family %in% c("poisson", "negbin", "gaussian")))
         rn <- gauss.quad(R, kind = 'hermite')
@@ -119,7 +113,6 @@ pglm <-  function(formula, data, subset, na.action,
                  rn = "rn")
     if (family %in% c("tobit", "gaussian", "poisson")) args$other <- "other"
     if (family == "negbin") args$vlink <- "vlink"
-    
     thefunc <- paste("function(start) lnl.", family,
                      "(", argschar(args), ")", sep = "")
     ml$logLik <- eval(parse(text = thefunc))
@@ -133,6 +126,7 @@ pglm <-  function(formula, data, subset, na.action,
     ml$start <- start
     ml[[1]] <- as.name('maxLik')
 #  if (family == "negbin") ml$activePar <- 14
+
     result <- eval(ml, parent.frame())
     result[c('call', 'args', 'model')] <- list(cl, args, data)
     result
